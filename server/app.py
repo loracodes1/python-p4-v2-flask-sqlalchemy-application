702d386ac2e5f2277cea7a516186a14663ba82ef
florence <loracodes3@gmail.com>
2025-01-21 23:51:16 +0300

    ddin views

--- server/app.py
+++ server/app.py
@@ -23,6 +23,15 @@ def index():
     )
     return response
 
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+    response_body = f'<p>{pet.name} {pet.species}</p>'
+
+    response = make_response(response_body, 200)
+
+    return response
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
